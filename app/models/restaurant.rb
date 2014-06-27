1302623d477fb8cc219106ba2f90826ec6776461
justine jones <justinejones@justines-MacBook-Air.local>
2014-06-27 14:34:47 -0400

    Adding flash alerts and notices

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,10 +2,7 @@ class Restaurant < ActiveRecord::Base
 	has_many :reservations
 	has_many :users, :through => :reservations
 
-	validates :name, :presence => true
-	validates :email, :presence => true
-	validates :address, :presence => true
-	validates :number, :summary, :presence => true 
-	validates :capacity, :presence => true, :numericality => {greater_than_or_equal_to: 20}
-	validates :price_range, :presence => true			
+	# belongs_to :owner, :class_name => "user" (now i need to add a owner_id column to the restuarants model)
+	validates :name,:email,:address,:number,:summary,:price_range, :presence => true
+	validates :capacity, :presence => true, :numericality => {greater_than_or_equal_to: 20}		
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,12 @@
 class User < ActiveRecord::Base
+	has_many :reservations
+	has_many :restaurants, :through => :reservations
+	#has_one :owned_restaurants, :class_name => "Restaurant", foreign_key => "owner_id"
+
 	has_secure_password
 
 	validates :email,
 						:name,
 						:presence => true
 	validates	:password, :length => {minimum: 6}
-
-	
-	has_many :reservations
-	has_many :restaurants, :through => :reservations
 end
