fb92ddf1653a6fa16b44e0a3d11009687d9240ac
Justine Jones <justinejones91@gmail.com>
2014-09-02 18:09:47 -0400

    fixed an error in the create action, changed current user to @restaurant

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,11 +1,11 @@
 class ReviewsController < ApplicationController
-	before_filter :load_restaurant, :except => [:index, :show, :edit]
+	before_filter :load_restaurant
 	before_filter :ensure_logged_in 
 
 
 	def create
 
-		@review = current_user.reviews.build(review_params)
+		@review = @restaurant.reviews.build(review_params)
 		@review.user_id = current_user.id
 
 
@@ -18,6 +18,7 @@ class ReviewsController < ApplicationController
 				format.html{render 'restaurants/show', alert: 'There was an error'}
 				format.js {}
 			end
+		end
 	end
 
 	def show
