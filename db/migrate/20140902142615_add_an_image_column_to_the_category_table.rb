1c69f6711232e73b44982e0660f69675db6247f4
Justine Jones <justinejones91@gmail.com>
2014-09-02 12:00:39 -0400

    added a new image column to the category model

---
+++ db/migrate/20140902142615_add_an_image_column_to_the_category_table.rb
@@ -0,0 +1,5 @@
+class AddAnImageColumnToTheCategoryTable < ActiveRecord::Migration
+  def change
+  	add_column :categories, :image, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140902134502) do
+ActiveRecord::Schema.define(version: 20140902142615) do
 
   create_table "categories", force: true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "summary"
     t.string   "cuisine"
+    t.string   "image"
   end
 
   create_table "reservations", force: true do |t|
