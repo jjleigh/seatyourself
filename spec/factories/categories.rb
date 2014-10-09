302c1d580366796e6e41c69d4f571a6dc1e0cb33
Justine Jones <justinejones91@gmail.com>
2014-10-09 12:25:17 -0400

    wrote passing tests for the category model

--- spec/factories/categories.rb
+++ spec/factories/categories.rb
@@ -2,7 +2,7 @@ require 'faker'
 
 FactoryGirl.define do
 	factory :category do
-		summary {Faker::Lorem.paragrapgh}
+		summary {Faker::Lorem.paragraph}
 		cuisine {Faker::Name.name}
 	end
 end
--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,1 +1,31 @@
 require 'rails_helper'
+
+
+describe Category do 
+
+	context "Before creation" do
+		it "has a valid factory" do 
+			user_one = build(:category)
+			expect(user_one).to be_valid
+		end 
+		# validation tests
+
+		it 'is invalid without a cuisine' do
+			expect(build(:category, cuisine: nil)).to_not be_valid
+		end
+
+
+		it 'is invalid without a summary' do
+			expect(FactoryGirl.build(:category, summary: nil)).to_not be_valid
+		end
+	
+	end
+
+	# association tests
+
+	it "should have many pictures" do 
+		expect(build(:category)).to respond_to(:restaurants)
+	end
+
+
+end
