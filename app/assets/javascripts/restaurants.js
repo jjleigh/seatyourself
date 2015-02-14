0e5791a1fc36e0102a5d5d0e2d2b2d25ea24487d
Justine Jones <justinejones91@gmail.com>
2015-02-14 17:35:10 -0500

    removed scrolling function

--- app/assets/javascripts/restaurants.js
+++
@@ -1,7 +0,0 @@
-
-var container = $('#review-list')
-var scrollTo = $('#review-list').last();
-
-container.scrollTop(
-	scrollTo.offset().top - container.offset().top + container.scrollTop()
-	);
--- app/assets/javascripts/reviews.js
+++ app/assets/javascripts/reviews.js
@@ -1,14 +1,2 @@
 // $('#review-list').last().css("background color", "yellow");
-$(window).scroll(function() {
-	var scrollTop = $(window).scrollTop();
-	if (scrollTop > $('#review-list').offset().top) {
-
-	}
-})
-
-var com = function() {
-	$('#review-list').(function() {
-		$(this).children()
-})
-}
 
